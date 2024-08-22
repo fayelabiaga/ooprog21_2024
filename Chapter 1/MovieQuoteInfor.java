bfe32488817598a4d6d9896dd06dfd8d6e2f29b4
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-08-22 00:06:39 -0700

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,14 @@
+public class MovieQuoteInfo {
+public static void main(String[] args) {
+
+    String quote = "Let it hurt then let it go.";
+    String movieName = "One Piece";
+    String characterName = "Monkey D Luffy and Nami";
+    int year = 1997;
+
+    System.out.println("Quote: " + quote);
+    System.out.println("Movie: " + movieName);
+    System.out.println("Character: " + characterName);
+    System.out.println("Year: " + year);
+    }
+}
