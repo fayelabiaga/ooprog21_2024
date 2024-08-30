39b1cd7f9d00596322aa124f00e53fc42ec55e83
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-08-30 08:47:44 +0700

    TableandChairs.java

---
+++ Chapter 1/TableandChairs.java
@@ -0,0 +1,10 @@
+public class TableAndChair {
+    public static void main(String[] args) {
+        System.out.println("x                        x");
+        System.out.println("x                        x");
+        System.out.println("x       xxxxxxxxxx       x");
+        System.out.println("xxxxxx  x        x  xxxxxx");
+        System.out.println("x    x  x        x  x    x");
+        System.out.println("x    x  x        x  x    x");
+    }
+}
