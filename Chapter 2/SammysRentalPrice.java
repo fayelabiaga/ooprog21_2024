836970fd990415a7627b82025bced989fcb07ab4
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-09-13 08:12:59 +0700

    SammysRentalPrice.java

--- Chapter 2/SammysRentalPrice.java
+++ Chapter 2/SammysRentalPrice.java
@@ -4,9 +4,9 @@ public class SammysRentalPrice {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int hourlyrate = 40;
-        int additionalminuterate= 1;
-        int minutesinhour = 60;
+        int hourlyRate = 40;
+        int additionalMinuteRate = 1;
+        int minutesInHour = 60;
 
         System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
         System.out.println("S    Sammy's makes it fun in the sun.      S");
@@ -14,11 +14,13 @@ public class SammysRentalPrice {
 
         System.out.print("Enter minutes rented: ");
         int totalMinutes = scanner.nextInt();
-        int hours = totalMinutes / minutesinhour;
-        int extraMinutes = totalMinutes % minutesinhour;
-        int totalPrice = hours * hourlyrate + extraMinutes * additionalminuterate;
+        int hours = totalMinutes / minutesInHour;
+        int extraMinutes = totalMinutes % minutesInHour;
+        int totalPrice = hours * hourlyRate + extraMinutes * additionalMinuteRate;
 
-        System.out.println("Hours: %d%nAdditional Minutes: %d%nTotal Price: $%d%n", hours, extraMinutes, totalPrice);
-
+        System.out.println("Hours: " + hours);
+        System.out.println("Additional Minutes: " + extraMinutes);
+        System.out.println("Total Price: $" + totalPrice);
+      
     }
 }
