152213bb1dbfba8d819f4207a99d319ca84e3925
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-09-13 08:11:49 +0700

    CarlysEventPrice.java

--- Chapter 2/CarlysEventPrice.java
+++ Chapter 2/CarlysEventPrice.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class CarlysEventPrice {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        int perguest = 35;
-        int largeevent = 50;
+        final int PER_GUEST = 35;
+        final int LARGE_EVENT_THRESHOLD = 50;
 
         System.out.println("************************************************");
         System.out.println("* Carly's makes the food that makes it a party.  *");
@@ -13,10 +12,13 @@ public class CarlysEventPrice {
 
         System.out.print("Enter number of guests: ");
         int guests = scanner.nextInt();
-        int totalPrice = guests * perguest;
 
-        System.out.printf("Number of guests: %d%nPrice per guest: $%d%nTotal price: $%d%n", guests, perguest, totalPrice);
-        System.out.printf("Large event: %b%n", guests >= largeevent);
+        int totalPrice = guests * PER_GUEST;
+        boolean isLargeEvent = guests >= LARGE_EVENT_THRESHOLD;
 
+            System.out.println("Number of guests: " + guests);
+            System.out.println("Price per guest: $" + PER_GUEST);
+            System.out.println("Total price: $" + totalPrice);
+            System.out.println("Large event: " + isLargeEvent);
+        }
     }
-}
