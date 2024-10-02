3b18727730e9db32363a95611aec96238ec59d9e
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-10-02 07:34:15 -0700

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,13 @@
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println("SQRT: " + Math.sqrt(37));
+        System.out.println("SIN: " + Math.sin(-1.57));
+        System.out.println("COS: " + Math.cos(1.57));
+        System.out.println("FLOOR: " + Math.floor(22.7));
+        System.out.println("CEIL: " + Math.ceil(22.3));
+        System.out.println("ROUND: " + Math.round(22.6));
+        System.out.println("MAX: " + Math.max(68, 71));
+        System.out.println("MIN: " + Math.min(68, 71));
+        System.out.println("RANDOM: " + (Math.random() * 46));
+    }
+}
