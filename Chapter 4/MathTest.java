9ec0a108859c6e4e4c203c22e37ec0747d05e72e
ROSHELE FAYE LABIAGA <labiagaroshelefaye@gmail.com>
2024-10-04 07:55:15 +0700

    MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,13 +1,19 @@
-public class MathTest {
-    public static void main(String[] args) {
-        System.out.println("SQRT: " + Math.sqrt(37));
-        System.out.println("SIN: " + Math.sin(-1.57));
-        System.out.println("COS: " + Math.cos(1.57));
+public class MathTest
+    {
+    public static void main(String[] args)
+        {
+        double num1 = 37.0; 
+        double angle = -1.57; 
+        double random = Math.random() * 100;
+
+        System.out.println("SQRT: " + Math.sqrt(num1));
+        System.out.println("SIN: " + Math.sin(angle));
+        System.out.println("COS: " + Math.cos(angle));
         System.out.println("FLOOR: " + Math.floor(22.7));
-        System.out.println("CEIL: " + Math.ceil(22.3));
-        System.out.println("ROUND: " + Math.round(22.6));
-        System.out.println("MAX: " + Math.max(68, 71));
-        System.out.println("MIN: " + Math.min(68, 71));
-        System.out.println("RANDOM: " + (Math.random() * 46));
+        System.out.println("CEIL: " + Math.ceil(22.7));
+        System.out.println("ROUND: " + Math.round(22.7));
+        System.out.println("MAX: " + Math.max(71, 68));
+        System.out.println("MIN: " + Math.min(71, 68));
+        System.out.println("RANDOM: " + random);
     }
 }
